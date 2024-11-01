aa9452703505bda808187daa379ebdb929140d10
Amarjeet <amarjeet.btechcse@gmail.com>
2024-11-01 22:10:02 +0530

    update on 240111

---
+++ 21. BACKTRACKING/N_queen_print_1_solution.java
@@ -0,0 +1,68 @@
+public class N_queen_print_1_solution {
+    public static boolean isSafe(char board[][], int row, int col) {
+        // Vertical up attack condition
+        for (int i = row - 1; i >= 0; i--) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+        // Left diagonal up condition
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        // Right diagonal up condition
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static boolean nQueens(char board[][], int row) {
+        // Base case
+        if (row == board.length) {
+            printBoard(board);
+            return true;
+        }
+
+        // Column loop
+        for (int j = 0; j < board.length; j++) {
+            if (isSafe(board, row, j)) { 
+                board[row][j] = 'Q'; // Place queen
+            if(nQueens(board, row + 1)){
+                return true;
+            } 
+                board[row][j] = 'X'; // Backtracking step
+            }
+        }
+        return false;
+    }
+    public static void printBoard(char board[][]) {
+        System.out.println("--- Chess Board Possible ---");
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 6; // Use n = 4 for testing since n = 2 has no solutions
+        char board[][] = new char[n][n];
+        // Initialize board with 'X'
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = 'X';
+            }
+        }
+        if(nQueens(board, 0)){
+            System.out.println("Solution is possible");
+            printBoard(board);
+        }else{
+            System.out.println("Solution is Not possible");
+        }
+    }
+}
